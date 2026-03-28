b5ab95a87dd7a7de4be72cdc282cdcbde8435600
YoannDev90 <yoanndev@outlook.fr>
2026-03-28 12:50:44 +0100

    feat: Configure basePath and assetPrefix for production environment

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
+  basePath: isProd ? "/Pollinations-Seed-Tier-Dev-Points" : "",
+  assetPrefix: isProd ? "/Pollinations-Seed-Tier-Dev-Points" : "",
   reactStrictMode: true,
 };
